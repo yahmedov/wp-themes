275ae92720bee22a2bebd2d7401ca3f20dc4a205
yahmedov <ahmedov.yuksel@gmail.com>
2016-01-15 15:06:55 +0200

    del changes

--- cannyon/cfg/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-get_template_part( 'cfg/cfgs' );
-get_template_part( 'cfg/default' );
-get_template_part( 'cfg/pages' );
-get_template_part( 'cfg/widgets' );
-?>
--- cannyon/cfg/widgets.php
+++
@@ -1,5 +0,0 @@
-<?php
-get_template_part( 'cfg/widgets/mythemes.post_meta.class' );
-get_template_part( 'cfg/widgets/mythemes.post_categories.class' );
-get_template_part( 'cfg/widgets/mythemes.post_tags.class' );
-?>
